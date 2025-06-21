cff501522a63eacf309d92cfc222d605ba77e1b8
jmelhamza <jmelhamza623@gmail.com>
2025-06-21 05:45:19 +0100

    set up react and awillwind

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+  
+  export default function App() {
+  return (
+    <div className="p-6 max-w-sm mx-auto bg-blue-200 rounded-xl shadow-md">
+      <h1 className="text-xl font-bold">React + Tailwind </h1>
+      <p>tailwind css have done </p>
+    </div>
+  );
+}
+
+  
