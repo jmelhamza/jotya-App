d54b701701beb5923bdc3d7b63d090b7b11be5d0
jmelhamza <jmelhamza623@gmail.com>
2026-05-18 13:03:16 +0100

    fix: App.jsx react component

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Jotiya App 🐱</h1>} />
+    </Routes>
+  );
+}
+
+export default App;
